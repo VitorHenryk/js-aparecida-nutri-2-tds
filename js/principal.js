b11214b3bebbc21afd634436a598fb6527968d52
Vitor Henryk <160151666+VitorHenryk@users.noreply.github.com>
2024-11-01 12:42:41 -0300

    Add files via upload

--- js/principal.js
+++ js/principal.js
@@ -38,6 +38,44 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 }
 
-console.log(peso); //Obter 100
-console.log(altura); //obter 2.00 metros
-console.log(imc); // obter 25 (peso / [altura x altura])
+titulo.addEventListener("click", mostraMensagem)
+function mostraMensagem() {
+    console.log("Olá eu fui clicado!");
+}
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function 
+(event){
+    event.preventDefault();
+    //console.log("oi, cliquei no botão");
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("td");
+
+    var nomeTd = document.createElement("tr");
+    var pesoTd = document.createElement("tr");
+    var alturaTd = document.createElement("tr");
+    var gorduraTd = document.createElement("tr");
+    var imcTd = document.createElement("tr");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
+})
